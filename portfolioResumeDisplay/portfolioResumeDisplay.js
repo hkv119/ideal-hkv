164e3b38ea7bda2c4e352163c9bfabe619241332
hkv119 <harivallepu119@gmail.com>
2025-04-30 13:07:02 -0500

    portfolioLWC

---
+++ portfolioResumeDisplay/portfolioResumeDisplay.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class PortfolioResumeDisplay extends LightningElement {
+      photoUrl = '/resource/profilePhoto'; // Upload image as Static Resource
+}
